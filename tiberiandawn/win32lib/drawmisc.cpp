04b96fb8e527cfcb6e9fe5d2b535e25656f6668e
Toni Spets <toni.spets@iki.fi>
2020-06-09 20:56:42 +0300

    Merge IconCache to common

--- tiberiandawn/win32lib/drawmisc.cpp
+++ tiberiandawn/win32lib/drawmisc.cpp
@@ -33,77 +33,7 @@
 
 #include "gbuffer.h"
 #include "MISC.H"
-
-IconCacheClass::IconCacheClass(void)
-{
-    IsCached = FALSE;
-    SurfaceLost = FALSE;
-    DrawFrequency = 0;
-    CacheSurface = NULL;
-    IconSource = NULL;
-}
-
-IconCacheClass::~IconCacheClass(void)
-{
-}
-
-IconCacheClass CachedIcons[MAX_CACHED_ICONS];
-
-extern "C" {
-IconSetType IconSetList[MAX_ICON_SETS];
-short IconCacheLookup[MAX_LOOKUP_ENTRIES];
-}
-
-int CachedIconsDrawn = 0;   // Counter of number of cache hits
-int UnCachedIconsDrawn = 0; // Counter of number of cache misses
-BOOL CacheMemoryExhausted;  // Flag set if we have run out of video RAM
-
-void Invalidate_Cached_Icons(void)
-{
-}
-void Restore_Cached_Icons(void)
-{
-}
-void Register_Icon_Set(void* icon_data, BOOL pre_cache){};
-
-//
-// Prototypes for assembly language procedures in STMPCACH.ASM
-//
-extern "C" void __cdecl Clear_Icon_Pointers(void){};
-extern "C" void __cdecl Cache_Copy_Icon(void const* icon_ptr, void*, int){};
-extern "C" int __cdecl Is_Icon_Cached(void const* icon_data, int icon)
-{
-    return -1;
-};
-extern "C" int __cdecl Get_Icon_Index(void* icon_ptr)
-{
-    return 0;
-};
-extern "C" int __cdecl Get_Free_Index(void)
-{
-    return 0;
-};
-extern "C" BOOL __cdecl Cache_New_Icon(int icon_index, void* icon_ptr)
-{
-    return -1;
-};
-extern "C" int __cdecl Get_Free_Cache_Slot(void)
-{
-    return -1;
-}
-
-void IconCacheClass::Draw_It(LPDIRECTDRAWSURFACE dest_surface,
-                             int x_pixel,
-                             int y_pixel,
-                             int window_left,
-                             int window_top,
-                             int window_width,
-                             int window_height)
-{
-}
-
-extern int CachedIconsDrawn;
-extern int UnCachedIconsDrawn;
+#include <tile.h>
 
 extern "C" void __cdecl Set_Font_Palette_Range(void const* palette, INT start_idx, INT end_idx)
 {
