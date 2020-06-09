70318604fab2806081f7a7b73728187755097e01
Toni Spets <toni.spets@iki.fi>
2020-06-09 17:32:43 +0300

    Merge Desired_Facing8 to common

--- common/miscasm.h
+++ common/miscasm.h
@@ -6,5 +6,6 @@ extern "C" int __cdecl calcy(signed short param1, short distance);
 extern "C" unsigned int __cdecl Cardinal_To_Fixed(unsigned base, unsigned cardinal);
 extern "C" unsigned int __cdecl Fixed_To_Cardinal(unsigned base, unsigned fixed);
 extern "C" int __cdecl Desired_Facing256(LONG srcx, LONG srcy, LONG dstx, LONG dsty);
+extern "C" int __cdecl Desired_Facing8(long x1, long y1, long x2, long y2);
 
 #endif
