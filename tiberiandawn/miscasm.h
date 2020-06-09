5c5bb80092c56f8cf4aa009beddf27cbc9d5cb4b
Toni Spets <toni.spets@iki.fi>
2020-06-09 20:03:20 +0300

    Move TD Fat_Put_Pixel to miscasma.asm

--- tiberiandawn/miscasm.h
+++ tiberiandawn/miscasm.h
@@ -1,7 +1,10 @@
 #ifndef MISCASM_H
 #define MISCASM_H
 
+class GraphicViewPortClass;
+
 extern "C" int __cdecl Distance_Coord(COORDINATE coord1, COORDINATE coord2);
 extern "C" CELL __cdecl Coord_Cell(COORDINATE coord);
+extern "C" void __cdecl Fat_Put_Pixel(int x, int y, int color, int siz, GraphicViewPortClass& gpage);
 
 #endif
