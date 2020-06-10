878072788c1c8beb7d442a10332fd99f83968a8f
Toni Spets <toni.spets@iki.fi>
2020-06-10 20:31:43 +0300

    Merge Buffer_Clear to common (sloppy):

--- common/drawmisc.h
+++ common/drawmisc.h
@@ -3,5 +3,6 @@
 
 extern "C" void __cdecl Buffer_Draw_Line(void* this_object, int sx, int sy, int dx, int dy, unsigned char color);
 extern "C" void __cdecl Buffer_Fill_Rect(void* thisptr, int sx, int sy, int dx, int dy, unsigned char color);
+extern "C" void __cdecl Buffer_Clear(void* this_object, unsigned char color);
 
 #endif
