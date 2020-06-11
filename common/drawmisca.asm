ed6e98908e1fd25d074e16ce2b7b53274d131833
Toni Spets <toni.spets@iki.fi>
2020-06-11 07:41:03 +0300

    Merge Buffer_To_Page to common (sloppy)

--- common/drawmisca.asm
+++ common/drawmisca.asm
@@ -31,6 +31,7 @@ externdef C Buffer_Put_Pixel:near
 externdef C Buffer_Get_Pixel:near
 externdef C Clip_Rect:near
 externdef C Confine_Rect:near
+externdef C Buffer_To_Page:near
 
 .code
 
@@ -2577,5 +2578,263 @@ Confine_Rect proc C x:dword, y:dword, w:dword, h:dword, widt:dword, height:dword
             pop ebx
             ret
 Confine_Rect endp
+
+;***************************************************************************
+;**   C O N F I D E N T I A L --- W E S T W O O D   A S S O C I A T E S   **
+;***************************************************************************
+;*                                                                         *
+;*                 Project Name : Westwood 32 bit Library                  *
+;*                                                                         *
+;*                    File Name : TOPAGE.ASM                               *
+;*                                                                         *
+;*                   Programmer : Phil W. Gorrow                           *
+;*                                                                         *
+;*                   Start Date : June 8, 1994                             *
+;*                                                                         *
+;*                  Last Update : June 15, 1994   [PWG]                    *
+;*                                                                         *
+;*-------------------------------------------------------------------------*
+;* Functions:                                                              *
+;*   Buffer_To_Page -- Copies a linear buffer to a virtual viewport	   *
+;* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - *
+
+;***************************************************************************
+;* VVC::TOPAGE -- Copies a linear buffer to a virtual viewport		   *
+;*                                                                         *
+;* INPUT:	WORD	x_pixel		- x pixel on viewport to copy from *
+;*		WORD	y_pixel 	- y pixel on viewport to copy from *
+;*		WORD	pixel_width	- the width of copy region	   *
+;*		WORD	pixel_height	- the height of copy region	   *
+;*		BYTE *	src		- buffer to copy from		   *
+;*		VVPC *  dest		- virtual viewport to copy to	   *
+;*                                                                         *
+;* OUTPUT:      none                                                       *
+;*                                                                         *
+;* WARNINGS:    Coordinates and dimensions will be adjusted if they exceed *
+;*	        the boundaries.  In the event that no adjustment is 	   *
+;*	        possible this routine will abort.  If the size of the 	   *
+;*		region to copy exceeds the size passed in for the buffer   *
+;*		the routine will automatically abort.			   *
+;*									   *
+;* HISTORY:                                                                *
+;*   06/15/1994 PWG : Created.                                             *
+;*=========================================================================*
+
+;extern "C" long __cdecl Buffer_To_Page(int x_pixel, int y_pixel, int pixel_width, int pixel_height, void* src, void* dest)
+Buffer_To_Page proc C x_pixel:dword, y_pixel:dword, pixel_width:dword, pixel_height:dword, src:dword, dest:dword
+
+        ;*===================================================================
+        ; Define some locals so that we can handle things quickly
+        ;*===================================================================
+        LOCAL     x1_pixel :dword
+        LOCAL    y1_pixel :dword
+        local    scr_x     : dword
+        local    scr_y     : dword
+        LOCAL    dest_ajust_width:DWORD
+        LOCAL    scr_ajust_width:DWORD
+        LOCAL    dest_area   :  dword
+
+        push ebx
+        push edi
+        push esi
+
+            cmp    [ src ] , 0
+            jz    real_out3
+
+
+        ; Clip dest Rectangle against source Window boundaries.
+
+            mov    [ scr_x ] , 0
+            mov    [ scr_y ] , 0
+            mov      esi , [ dest ]        ; get ptr to dest
+            xor     ecx , ecx
+            xor     edx , edx
+            mov    edi , (GraphicViewPort ptr [esi]).GVPWidth  ; get width into register
+            mov    ebx , [ x_pixel ]
+            mov    eax , [ x_pixel ]
+            add    ebx , [ pixel_width ]
+            shld    ecx , eax , 1
+            mov    [ x1_pixel ] , ebx
+            inc    edi
+            shld    edx , ebx , 1
+            sub    eax , edi
+            sub    ebx , edi
+            shld    ecx , eax , 1
+            shld    edx , ebx , 1
+
+            mov    edi, (GraphicViewPort ptr [esi]).GVPHeight ; get height into register
+            mov    ebx , [ y_pixel ]
+            mov    eax , [ y_pixel ]
+            add    ebx , [ pixel_height ]
+            shld    ecx , eax , 1
+            mov    [ y1_pixel ] , ebx
+            inc    edi
+            shld    edx , ebx , 1
+            sub    eax , edi
+            sub    ebx , edi
+            shld    ecx , eax , 1
+            shld    edx , ebx , 1
+
+            xor    cl , 5
+            xor    dl , 5
+            mov    al , cl
+            test    dl , cl
+            jnz    real_out3
+            or    al , dl
+            jz    do_blit2
+
+            test    cl , 1000b
+            jz    dest_left_ok2
+            mov    eax , [ x_pixel ]
+            neg    eax
+            mov    [ x_pixel ] , 0
+            mov    [ scr_x ] , eax
+
+        dest_left_ok2:
+            test    cl , 0010b
+            jz    dest_bottom_ok2
+            mov    eax , [ y_pixel ]
+            neg    eax
+            mov    [ y_pixel ] , 0
+            mov    [ scr_y ] , eax
+
+        dest_bottom_ok2:
+            test    dl , 0100b
+            jz    dest_right_ok2
+            mov    eax , (GraphicViewPort ptr [esi]).GVPWidth  ; get width into register
+            mov    [ x1_pixel ] , eax
+        dest_right_ok2:
+            test    dl , 0001b
+            jz    do_blit2
+            mov    eax , (GraphicViewPort ptr [esi]).GVPHeight  ; get width into register
+            mov    [ y1_pixel ] , eax
+
+        do_blit2:
+
+               cld
+
+               mov    eax , (GraphicViewPort ptr [esi]).GVPXAdd
+               add    eax , (GraphicViewPort ptr [esi]).GVPWidth
+               add    eax , (GraphicViewPort ptr [esi]).GVPPitch
+               mov    edi , (GraphicViewPort ptr [esi]).GVPOffset
+
+               mov    ecx , eax
+               mul    [ y_pixel ]
+               add    edi , [ x_pixel ]
+               add    edi , eax
+
+               add    ecx , [ x_pixel ]
+               sub    ecx , [ x1_pixel ]
+               mov    [ dest_ajust_width ] , ecx
+
+
+               mov    esi , [ src ]
+               mov    eax , [ pixel_width ]
+               sub    eax , [ x1_pixel ]
+               add    eax , [ x_pixel ]
+               mov    [ scr_ajust_width ] , eax
+
+               mov    eax , [ scr_y ]
+               mul     [ pixel_width ]
+               add    eax , [ scr_x ]
+               add    esi , eax
+
+               mov    edx , [ y1_pixel ]
+               mov    eax , [ x1_pixel ]
+
+               sub    edx , [ y_pixel ]
+               jle    real_out3
+               sub    eax , [ x_pixel ]
+               jle    real_out3
+
+
+        ; ********************************************************************
+        ; Forward bitblit only
+
+        ; IF TRANSP
+        ;        test    [ trans ] , 1
+        ;        jnz    forward_Blit_trans
+        ; ENDIF
+
+
+        ; the inner loop is so efficient that
+        ; the optimal consept no longer apply because
+        ; the optimal byte have to by a number greather than 9 bytes
+               cmp    eax , 10
+               jl    forward_loop_bytes2
+
+        forward_loop_dword2:
+               mov    ecx , edi
+               mov    ebx , eax
+               neg    ecx
+               and    ecx , 3
+               sub    ebx , ecx
+               rep    movsb
+               mov    ecx , ebx
+               shr    ecx , 2
+               rep    movsd
+               mov    ecx , ebx
+               and    ecx , 3
+               rep    movsb
+               add    esi , [ scr_ajust_width ]
+               add    edi , [ dest_ajust_width ]
+               dec    edx
+               jnz    forward_loop_dword2
+               jmp    real_out3 ; ret
+
+        forward_loop_bytes2:
+               mov    ecx , eax
+               rep    movsb
+               add    esi , [ scr_ajust_width ]
+               add    edi , [ dest_ajust_width ]
+               dec    edx                    ; decrement the height
+               jnz    forward_loop_bytes2
+            ;  ret
+
+            ;/ IF  TRANSP
+            ;/
+            ;/
+            ;/ forward_Blit_trans:
+            ;/
+            ;/
+            ;/        mov    ecx , eax
+            ;/        and    ecx , 01fh
+            ;/        lea    ecx , [ ecx + ecx * 4 ]
+            ;/        neg    ecx
+            ;/        shr    eax , 5
+            ;/        lea    ecx , [ transp_reference + ecx * 2 ]
+            ;/        mov    [ y1_pixel ] , ecx
+            ;/
+            ;/
+            ;/ forward_loop_trans:
+            ;/        mov    ecx , eax
+            ;/        jmp    [ y1_pixel ]
+            ;/ forward_trans_line:
+            ;/        REPT    32
+            ;/        local    transp_pixel
+            ;/                mov    bl , [ esi ]
+            ;/                inc    esi
+            ;/                test    bl , bl
+            ;/                jz    transp_pixel
+            ;/                mov    [ edi ] , bl
+            ;/             transp_pixel:
+            ;/                inc    edi
+            ;/    ENDM
+            ;/     transp_reference:
+            ;/        dec    ecx
+            ;/        jge    forward_trans_line
+            ;/        add    esi , [ scr_ajust_width ]
+            ;/        add    edi , [ dest_ajust_width ]
+            ;/        dec    edx
+            ;/        jnz    forward_loop_trans
+            ;/        ret
+            ;/ ENDIF
+
+        real_out3:
+            pop esi
+            pop edi
+            pop ebx
+            ret
+Buffer_To_Page endp
 
 end
