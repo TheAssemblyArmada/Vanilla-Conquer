d6d7ed7618bf8d25f600e3ed353e7418acbedadb
Toni Spets <toni.spets@iki.fi>
2020-06-11 07:13:49 +0300

    Merge palette variables to common

--- common/drawmisc.h
+++ common/drawmisc.h
@@ -1,6 +1,9 @@
 #ifndef DRAWMISC_H
 #define DRAWMISC_H
 
+extern "C" extern unsigned char CurrentPalette[768];
+extern "C" extern unsigned char PaletteTable[1024];
+
 extern "C" void __cdecl Buffer_Draw_Line(void* this_object, int sx, int sy, int dx, int dy, unsigned char color);
 extern "C" void __cdecl Buffer_Fill_Rect(void* thisptr, int sx, int sy, int dx, int dy, unsigned char color);
 extern "C" void __cdecl Buffer_Clear(void* this_object, unsigned char color);
