38396f8d921966d40fd61c3069301ea87efba4aa
OmniBlade <nadia_xy@hotmail.com>
2020-06-20 22:57:30 +0100

    Replaces broken Linear_Blit_To_Linear with C implementation.
    Unclear what issue is with asm version as asm looks correct?

---
+++ common/bitblit.h
@@ -0,0 +1,22 @@
+#ifndef BITBLIT_H
+#define BITBLIT_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+int __cdecl Linear_Blit_To_Linear(void* thisptr,
+                                  void* dest,
+                                  int x_pixel,
+                                  int y_pixel,
+                                  int dx_pixel,
+                                  int dy_pixel,
+                                  int pixel_width,
+                                  int pixel_height,
+                                  int trans);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
