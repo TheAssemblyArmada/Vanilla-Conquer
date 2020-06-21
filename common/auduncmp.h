f7da6ffda0f7420781edb2d9431341b70c0a563f
OmniBlade <nadia_xy@hotmail.com>
2020-06-21 20:55:47 +0100

    Implements decoding of "wsaud" type audio with "Audio_Unzap" function.

---
+++ common/auduncmp.h
@@ -0,0 +1,14 @@
+#ifndef AUDUNCMP_H
+#define AUDUNCMP_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+short __cdecl Audio_Unzap(void* source, void* dest, short size);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
