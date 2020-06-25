fbff2f39f50bc10c3a44430d0989074b325dcc1c
Toni Spets <toni.spets@iki.fi>
2020-06-25 21:01:52 +0300

    Import (unsafe) versions of Chronoshift bit funcs

--- common/misc.c
+++ common/misc.c
@@ -115,4 +115,92 @@ int __cdecl Desired_Facing8(long x1, long y1, long x2, long y2)
     return dirtype + 32;
 }
 
+void __cdecl Set_Bit(void* array, int bit, int value)
+{
+    // Don't try and handle a negative value
+    if (bit < 0) {
+        return;
+    }
+
+    uint8_t *byte_array = (uint8_t *)array;
+
+    if (value) {
+        byte_array[bit / 8] |= 1 << (bit % 8);
+    } else {
+        byte_array[bit / 8] &= ~(1 << (bit % 8));
+    }
+}
+
+int __cdecl Get_Bit(void const* array, int bit)
+{
+    // If negative it is out of range so can't be set
+    if (bit < 0) {
+        return 0;
+    }
+
+    const uint8_t *byte_array = (const uint8_t *)array;
+
+    return byte_array[bit / 8] & (1 << (bit % 8)) ? 1 : 0;
+}
+
+int __cdecl First_True_Bit(void const* array)
+{
+    const uint8_t *byte_array = (const uint8_t *)array;
+
+    int bytenum = 0;
+    int bitnum = 0;
+
+    // Find the first none zero byte as it must contain the first bit.
+    for (bytenum = 0; /*bytenum < size*/; ++bytenum) {
+        if (byte_array[bytenum] != 0) {
+            break;
+        }
+    }
+
+#if 0
+    if (bytenum >= size) {
+        return 8 * bytenum;
+    }
+#endif
+
+    // Scan through the bits of the byte until we find the first set bit.
+    for (bitnum = 0; bitnum < 8; ++bitnum) {
+        if (Get_Bit(&byte_array[bytenum], bitnum)) {
+            break;
+        }
+    }
+
+    return 8 * bytenum + bitnum;
+}
+
+int __cdecl First_False_Bit(void const* array)
+{
+    const uint8_t *byte_array = (const uint8_t *)array;
+
+    int bytenum = 0;
+    int bitnum = 0;
+
+    // Find the first byte with an unset bit as it must contain the first bit.
+    for (bytenum = 0; /*bytenum < size*/; ++bytenum) {
+        if (byte_array[bytenum] != 0xFF) {
+            break;
+        }
+    }
+
+#if 0
+    if (bytenum >= size) {
+        return 8 * bytenum;
+    }
+#endif
+
+    // Scan through the bits until we find one that isn't set.
+    for (bitnum = 0; bitnum < 8; ++bitnum) {
+        if (!Get_Bit(&byte_array[bytenum], bitnum)) {
+            break;
+        }
+    }
+
+    return 8 * bytenum + bitnum;
+}
+
 #endif
