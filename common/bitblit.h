63799d5bc7168fefe6aca26f12a857d5747db7c8
Toni Spets <toni.spets@iki.fi>
2020-07-25 21:56:47 +0300

    Bitblit type cleanup

--- common/bitblit.h
+++ common/bitblit.h
@@ -5,15 +5,15 @@
 extern "C" {
 #endif
 
-int __cdecl Linear_Blit_To_Linear(void* thisptr,
-                                  void* dest,
-                                  int x_pixel,
-                                  int y_pixel,
-                                  int dx_pixel,
-                                  int dy_pixel,
-                                  int pixel_width,
-                                  int pixel_height,
-                                  int trans);
+int Linear_Blit_To_Linear(void* thisptr,
+                          void* dest,
+                          int x_pixel,
+                          int y_pixel,
+                          int dx_pixel,
+                          int dy_pixel,
+                          int pixel_width,
+                          int pixel_height,
+                          int trans);
 
 #ifdef __cplusplus
 }
