d1c58984cb719686caccea06b6a8b9f5851cbbce
Toni Spets <toni.spets@iki.fi>
2020-07-25 21:56:56 +0300

    Setfpal type cleanup

--- common/setfpal.c
+++ common/setfpal.c
@@ -2,7 +2,7 @@
 
 extern unsigned char ColorXlat[16][16];
 
-void __cdecl Set_Font_Palette_Range(void const* palette, int start_idx, int end_idx)
+void Set_Font_Palette_Range(void const* palette, int start_idx, int end_idx)
 {
     start_idx &= 15;
     end_idx &= 15;
