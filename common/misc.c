dc6658aae2c448a10168992919c7fa5517628b21
OmniBlade <nadia_xy@hotmail.com>
2020-07-30 14:51:54 +0100

    Fixes edge cases with calcx/y.

--- common/misc.c
+++ common/misc.c
@@ -1,24 +1,18 @@
 #include "miscasm.h"
 #include <stdbool.h>
 
-unsigned short __rotl16(unsigned short a, unsigned b)
-{
-    b &= 15;
-    return (a << b) | (a >> (16 - b));
-}
-
 #ifdef NOASM
 
 int calcx(signed short param1, short distance)
 {
     int tmp = (int)param1 * distance;
-    return (((unsigned short)tmp / 128) & 0xFF) | ((__rotl16(tmp >> 16, 1) << 8) & 0xFF00);
+    return (unsigned short)(tmp >> 7);
 }
 
 int calcy(signed short param1, short distance)
 {
     int tmp = (int)param1 * distance;
-    return -((((unsigned short)tmp / 128) & 0xFF) | ((__rotl16(tmp >> 16, 1) << 8) & 0xFF00));
+    return -((unsigned short)(tmp >> 7));
 }
 
 unsigned int Cardinal_To_Fixed(unsigned int base, unsigned int cardinal)
