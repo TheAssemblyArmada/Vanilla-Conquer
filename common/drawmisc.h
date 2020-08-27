eb914621fe05c034f4a6459bc2904a9a76cf6bde
Toni Spets <toni.spets@iki.fi>
2020-08-27 19:05:18 +0100

    Fixes test_fatpixel on none windows.

--- common/drawmisc.h
+++ common/drawmisc.h
@@ -1,46 +1,49 @@
 #ifndef DRAWMISC_H
 #define DRAWMISC_H
 
-extern "C" extern unsigned char CurrentPalette[768];
-extern "C" extern unsigned char PaletteTable[1024];
-
-extern "C" void __cdecl Buffer_Draw_Line(void* this_object, int sx, int sy, int dx, int dy, unsigned char color);
-extern "C" void __cdecl Buffer_Fill_Rect(void* thisptr, int sx, int sy, int dx, int dy, unsigned char color);
-extern "C" void __cdecl Buffer_Clear(void* this_object, unsigned char color);
-extern "C" bool __cdecl Linear_Blit_To_Linear(void* this_object,
-                                              void* dest,
-                                              int x_pixel,
-                                              int y_pixel,
-                                              int dest_x0,
-                                              int dest_y0,
-                                              int pixel_width,
-                                              int pixel_height,
-                                              bool trans);
-extern "C" bool __cdecl Linear_Scale_To_Linear(void* this_object,
-                                               void* dest,
-                                               int src_x,
-                                               int src_y,
-                                               int dst_x,
-                                               int dst_y,
-                                               int src_width,
-                                               int src_height,
-                                               int dst_width,
-                                               int dst_height,
-                                               bool trans,
-                                               char* remap);
-
-extern "C" extern int LastIconset;
-extern "C" extern int StampPtr;
-extern "C" extern int IsTrans;
-extern "C" extern int MapPtr;
-extern "C" extern int IconWidth;
-extern "C" extern int IconHeight;
-extern "C" extern int IconSize;
-extern "C" extern int IconCount;
-
-extern "C" void __cdecl Init_Stamps(unsigned int icondata);
-
 class GraphicViewPortClass;
-extern "C" void Fat_Put_Pixel(int x, int y, int value, int size, GraphicViewPortClass& gvp);
+
+extern "C" {
+extern unsigned char CurrentPalette[768];
+extern unsigned char PaletteTable[1024];
+
+void Buffer_Draw_Line(void* this_object, int sx, int sy, int dx, int dy, unsigned char color);
+void Buffer_Fill_Rect(void* thisptr, int sx, int sy, int dx, int dy, unsigned char color);
+void Buffer_Clear(void* this_object, unsigned char color);
+bool Linear_Blit_To_Linear(void* this_object,
+                           void* dest,
+                           int x_pixel,
+                           int y_pixel,
+                           int dest_x0,
+                           int dest_y0,
+                           int pixel_width,
+                           int pixel_height,
+                           bool trans);
+bool Linear_Scale_To_Linear(void* this_object,
+                            void* dest,
+                            int src_x,
+                            int src_y,
+                            int dst_x,
+                            int dst_y,
+                            int src_width,
+                            int src_height,
+                            int dst_width,
+                            int dst_height,
+                            bool trans,
+                            char* remap);
+
+extern int LastIconset;
+extern int StampPtr;
+extern int IsTrans;
+extern int MapPtr;
+extern int IconWidth;
+extern int IconHeight;
+extern int IconSize;
+extern int IconCount;
+
+void Init_Stamps(unsigned int icondata);
+
+void Fat_Put_Pixel(int x, int y, int value, int size, GraphicViewPortClass& gvp);
+}
 
 #endif
