a80313de89e7af6fcf0d9cccdf6387d42291a7f7
OmniBlade <nadia_xy@hotmail.com>
2020-09-28 00:01:06 +0100

    Implements Buffer_Draw_Line in C++.

--- common/drawmisc.h
+++ common/drawmisc.h
@@ -7,7 +7,6 @@ extern "C" {
 extern unsigned char CurrentPalette[768];
 extern unsigned char PaletteTable[1024];
 
-void Buffer_Draw_Line(void* this_object, int sx, int sy, int dx, int dy, unsigned char color);
 void Buffer_Fill_Rect(void* thisptr, int sx, int sy, int dx, int dy, unsigned char color);
 void Buffer_Clear(void* this_object, unsigned char color);
 bool Linear_Blit_To_Linear(void* this_object,
