878178f6d9b711c72d46e67a30b5c688627fd6c4
OmniBlade <nadia_xy@hotmail.com>
2021-01-09 00:27:55 +0000

    [TD] Pause the game when options menu is open in skirmish.

--- tiberiandawn/goptions.cpp
+++ tiberiandawn/goptions.cpp
@@ -211,7 +211,7 @@ void GameOptionsClass::Process(void)
         /*
         **	Invoke game callback.
         */
-        if (GameToPlay == GAME_NORMAL) {
+        if (GameToPlay == GAME_NORMAL || GameToPlay == GAME_SKIRMISH) {
             Call_Back();
         } else {
             if (Main_Loop()) {
