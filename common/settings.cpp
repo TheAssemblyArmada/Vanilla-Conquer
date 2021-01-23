06fee3a3eedb11ce2af1e3bff93e5d020c56eaef
Toni Spets <toni.spets@iki.fi>
2021-01-23 18:31:03 +0200

    Automatic boxing for SDL2

--- common/settings.cpp
+++ common/settings.cpp
@@ -10,6 +10,7 @@ SettingsClass::SettingsClass()
     Video.Windowed = false;
     Video.Width = 0;
     Video.Height = 0;
+    Video.Boxing = false;
     Video.FrameLimit = 120;
     Video.HardwareCursor = true;
     Video.Scaler = "nearest";
@@ -22,6 +23,7 @@ void SettingsClass::Load(INIClass& ini)
     Video.WindowWidth = ini.Get_Int("Video", "WindowWidth", Video.WindowWidth);
     Video.WindowHeight = ini.Get_Int("Video", "WindowHeight", Video.WindowHeight);
     Video.Windowed = ini.Get_Bool("Video", "Windowed", Video.Windowed);
+    Video.Boxing = ini.Get_Bool("Video", "Boxing", Video.Boxing);
     Video.Width = ini.Get_Int("Video", "Width", Video.Width);
     Video.Height = ini.Get_Int("Video", "Height", Video.Height);
     Video.FrameLimit = ini.Get_Int("Video", "FrameLimit", Video.FrameLimit);
@@ -29,6 +31,13 @@ void SettingsClass::Load(INIClass& ini)
     Video.Scaler = ini.Get_String("Video", "Scaler", Video.Scaler);
     Video.Driver = ini.Get_String("Video", "Driver", Video.Driver);
     Video.PixelFormat = ini.Get_String("Video", "PixelFormat", Video.PixelFormat);
+
+    /*
+    ** Boxing requires software cursor.
+    */
+    if (Video.Boxing) {
+        Video.HardwareCursor = false;
+    }
 }
 
 void SettingsClass::Save(INIClass& ini)
@@ -36,6 +45,7 @@ void SettingsClass::Save(INIClass& ini)
     ini.Put_Int("Video", "WindowWidth", Video.WindowWidth);
     ini.Put_Int("Video", "WindowHeight", Video.WindowHeight);
     ini.Put_Bool("Video", "Windowed", Video.Windowed);
+    ini.Put_Bool("Video", "Boxing", Video.Boxing);
     ini.Put_Int("Video", "Width", Video.Width);
     ini.Put_Int("Video", "Height", Video.Height);
     ini.Put_Int("Video", "FrameLimit", Video.FrameLimit);
