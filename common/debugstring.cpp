05f98ea4e5a0eae8b5e5a96332402392c58fc9dc
OmniBlade <nadia_xy@hotmail.com>
2021-01-25 22:48:44 +0000

    Slightly improves the debug string win32 code.
    Allows redirecting debug output from the console to files on disk for example.

--- common/debugstring.cpp
+++ common/debugstring.cpp
@@ -6,6 +6,7 @@
 
 #ifdef _WIN32
 #include <windows.h>
+#include <io.h>
 #endif
 
 static class DebugStateClass
@@ -18,10 +19,18 @@ public:
 #ifdef _WIN32
         /* Attach to the console that started us if any */
         if (AttachConsole(ATTACH_PARENT_PROCESS)) {
-            /* We attached successfully, lets redirect IO to the consoles handles */
-            freopen("CONIN$", "r", stdin);
-            freopen("CONOUT$", "w", stdout);
-            freopen("CONOUT$", "w", stderr);
+            /* We attached successfully, lets redirect IO to the consoles handles if not already redirected */
+            if (_fileno(stdout) == -2 || _get_osfhandle(fileno(stdout)) == -2) {
+                freopen("CONOUT$", "w", stdout);
+            }
+
+            if (_fileno(stderr) == -2 || _get_osfhandle(fileno(stderr)) == -2) {
+                freopen("CONOUT$", "w", stderr);
+            }
+
+            if (_fileno(stdin) == -2 || _get_osfhandle(fileno(stdin)) == -2) {
+                freopen("CONIN$", "r", stdin);
+            }
         }
 #endif
     }
