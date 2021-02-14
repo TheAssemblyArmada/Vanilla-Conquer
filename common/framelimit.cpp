8202c97f2b9ccdac597d1af8f6f536cf9e4b49d9
Toni Spets <toni.spets@iki.fi>
2021-02-14 15:39:37 +0200

    Fix frame limiter
    Fixed an issue where the frametime was incorrectly calculated
    before sleeping making the games get less accurate ticks when the
    frame limit was low. This also caused the renders to happen
    erratically and fps being doubled.
    
    Introduced frame dropping concept where non-critical frames can be
    dropped but game ticks force render. This should smoothen out
    lower specced systems.

--- common/framelimit.cpp
+++ common/framelimit.cpp
@@ -15,21 +15,41 @@ extern WWMouseClass* WWMouse;
 void Video_Render_Frame();
 #endif
 
-void Frame_Limiter()
+void Frame_Limiter(bool force_render)
 {
     static auto frame_start = std::chrono::steady_clock::now();
 #ifdef SDL2_BUILD
+    static auto render_avg = 0;
+
+    auto render_start = std::chrono::steady_clock::now();
+    auto render_remaining = std::chrono::duration_cast<std::chrono::milliseconds>(frame_start - render_start).count();
+
+    if (force_render == false && render_remaining > render_avg) {
+        ms_sleep(unsigned(render_remaining));
+        return;
+    }
+
     Video_Render_Frame();
+
+    auto render_end = std::chrono::steady_clock::now();
+    auto render_time = std::chrono::duration_cast<std::chrono::milliseconds>(render_end - render_start).count();
+
+    // keep up some average so we have an idea if we need to skip a frame or not
+    render_avg = (render_avg + render_time) / 2;
 #endif
 
     if (Settings.Video.FrameLimit > 0) {
+#ifdef SDL2_BUILD
+        auto frame_end = render_end;
+#else
         auto frame_end = std::chrono::steady_clock::now();
+#endif
         int64_t _ms_per_tick = 1000 / Settings.Video.FrameLimit;
         auto remaining =
             _ms_per_tick - std::chrono::duration_cast<std::chrono::milliseconds>(frame_end - frame_start).count();
         if (remaining > 0) {
             ms_sleep(unsigned(remaining));
         }
-        frame_start = frame_end;
+        frame_start = std::chrono::steady_clock::now();
     }
 }
