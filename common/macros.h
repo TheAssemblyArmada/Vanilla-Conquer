0390974ed2010ff837bc88bbefee892991b65280
OmniBlade <nadia_xy@hotmail.com>
2021-04-29 22:42:16 +0100

    Silences macro redefinition warning.

---
+++ common/macros.h
@@ -0,0 +1,22 @@
+// TiberianDawn.DLL and RedAlert.dll and corresponding source code is free
+// software: you can redistribute it and/or modify it under the terms of
+// the GNU General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+
+// TiberianDawn.DLL and RedAlert.dll and corresponding source code is distributed
+// in the hope that it will be useful, but with permitted additional restrictions
+// under Section 7 of the GPL. See the GNU General Public License in LICENSE.TXT
+// distributed with this program. You should have received a copy of the
+// GNU General Public License along with permitted additional restrictions
+// with this program. If not, see https://github.com/electronicarts/CnC_Remastered_Collection
+
+#ifndef COMMON_MACROS_H
+#define COMMON_MACROS_H
+
+/*
+**	This macro serves as a general way to determine the number of elements
+**	within an array.
+*/
+#define ARRAY_SIZE(x) int(sizeof(x) / sizeof(x[0]))
+
+#endif /* COMMON_MACROS_H */
